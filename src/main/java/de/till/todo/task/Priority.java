18ec10c52a82061362a9272ca5d7b62eb1e8645c
tillweckheuer <till.weckheuer@aol.com>
2025-02-07 23:45:42 +0100

    updates Priority and Status enum

--- src/main/java/de/till/todo/task/Priority.java
+++ src/main/java/de/till/todo/task/Priority.java
@@ -1,7 +1,20 @@
 package de.till.todo.task;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Priority {
-    LOW,
-    MEDIUM,
-    HIGH
+    LOW(0),
+    MEDIUM(1),
+    HIGH(2);
+
+    private final int value;
+
+    Priority(int value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public int getValue() {
+        return value;
+    }
 }
--- src/main/java/de/till/todo/task/Status.java
+++ src/main/java/de/till/todo/task/Status.java
@@ -1,7 +1,20 @@
 package de.till.todo.task;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Status {
-    TODO,
-    IN_PROGRESS,
-    DONE
+    TODO(0),
+    IN_PROGRESS(1),
+    DONE(2);
+
+    private final int value;
+
+    Status(int value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public int getValue() {
+        return value;
+    }
 }
